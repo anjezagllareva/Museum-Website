31d7f5cde94e08974aa3da016a7f9d549d5b901f
ErzaBoshnjaku <erzaboshnjaku28@gmail.com>
2023-02-20 18:26:58 +0100

    About us User

---
+++ Views/AboutUsUser.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Museum</title>
+    <link rel="stylesheet" href="AboutUsUserStyle.php">
+</head>
+<body>
+    <header><a href="Homeuser.php">MuseumEX</a></header>
+
+
+<div class="about-section">
+  <h1>About Us Page</h1>
+  <p>We represent the most beautiful museums around the world.</p>
+  <p>We like to share what those buildings hold and the collections of objects that are found in nature or objects that people created. Many museums focus on a single subject for example, art, science, or history,but we are open for all the ideas you want to share with us and we will share for the people that follow our steps </p>
+</div>
+
+<h2 style="text-align:center">Our Team</h2>
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="Anjeza.jpg" alt="Anjeza" style="width:60%">
+      <div class="container">
+        <h2>Anjeza Gllareva</h2>
+        <p class="title">CEO & Founder</p>
+        <p>"No woman can be successful, unless she first loves her work."</p>
+        <p>Anjeza@gmail.com</p>
+        <p><button class="button" onclick= "location.href='Contact.php'">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="Erza.jfif" alt="Erza" style="width:60%">
+      <div class="container">
+        <h2>Erza Boshnjaku</h2>
+        <p class="title">Manager</p>
+        <p>“High expectations are the key to absolutely everything.”</p>
+        <p>Erza@gmail.com</p>
+        <p><button class="button" onclick= "location.href='Contact.php'">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+</body>
+
+
+    </html>
