b702bfd3e68e581f007be34305b9ee1dde3ce4a9
ErzaBoshnjaku <erzaboshnjaku28@gmail.com>
2023-02-20 20:45:11 +0100

    Contact style

---
+++ Views/Contactstyle.php
@@ -0,0 +1,70 @@
+<?php
+header('Content-type: text/css; charset:UTF-8');
+?>
+@font-face {
+    font-family: unbounded;
+    src: url(Unbounded-VariableFont_wght.ttf);
+  }
+header a{
+    color: white;
+    font-weight: 140px;
+    font-family: unbounded;
+    text-shadow: 3px 3px black;
+    font-size: 60px;
+    margin-top: 15px;
+    margin-left: 10px;
+    text-decoration: none;
+}
+body{
+background-color: #171c24;
+}
+.contact-form{
+  background:#997300;
+  border:2px solid #fff;
+  margin-left: 160px;
+  margin-top: 10%;
+  margin-bottom: 5%;
+  width: 70%;
+}
+.contact-form .form-control{
+  border-radius:1rem;
+  width:300px;
+  height:35px;
+  margin-left: 160px;
+  text-align: center;
+  margin-top: 10px;
+}
+.contact-form .form1-control{
+  border-radius:3rem;
+  border:2px solid black;
+  text-align: center;
+  width:300px;
+  height:10px;
+  margin-top: 10px;
+}
+
+.contact-image{
+  text-align: center;
+
+}
+.contact-image img{
+  border-radius: 100%;
+  width: 11%;
+  box-shadow: 2px 2px 2px 2px white;
+  margin-top: -3%;
+  transform: rotate(3deg);
+}
+.contact-form form{
+  padding: 14%;
+}
+.contact-form form .row{
+  margin-bottom: -7%;
+}
+.contact-form h3{
+  margin-bottom: 8%;
+  margin-top: -10%;
+  text-align: center;
+  color: #171c24;
+  font-size: 30px;
+  font-family: unbounded;
+}
