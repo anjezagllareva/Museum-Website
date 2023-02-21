bb65c0acc584c4f1ae601c63b600e737a487a1c4
anjezagllareva <anjezagllarevaa@gmail.com>
2023-02-21 22:38:58 +0100

    controlleri per produktet

---
+++ Controllers/ProductsController.php
@@ -0,0 +1,22 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD']==='POST')
+{
+    if(isset($_POST['Save']))
+    {
+        $name = $_POST['Name'];
+        $author = $_POST['Author'];
+        $description = $_POST['Description'];
+        $image = $_POST['Image'];
+        $price = $_POST['Price'];
+        $insertedBy = $_POST['InsertedBy'];
+
+        header("Location:Products.php");
+
+        $product  = new Product($name,$author,$description,$image,$price,$insertedBy);
+        $productService = new ProductService();
+    
+        $productService->createProduct($product);
+    }
+}
+?>
