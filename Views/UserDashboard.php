be4b928a0f6ae6cced52e9e879fd0f0907cb76e1
anjezagllareva <anjezagllarevaa@gmail.com>
2023-02-21 22:31:08 +0100

    dashboard

---
+++ Views/UserDashboard.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Museum</title>
+    <link rel="stylesheet" href="UsersDashboardStyle.php">
+</head>
+<body>
+  <header><a href="HomeAdmin.php">MuseumEX</a></header>
+<div class="navbar">
+    <a href="HomeAdmin.php">HOME</a>
+    <div class="dropdown">
+      <button class="dropbtn">DASHBOARD
+      </button>
+      <div class="dropdown-content">
+        <a href="UsersDashboard.php">Users</a>
+        <a href="MessagesDashboard.php">Messages</a>
+        <a href="ProductsDashboard.php">Products</a>
+      </div>
+    </div>
+    <div class="dropdown">
+      <button class="dropbtn">MUSEUMS
+      </button>
+      <div class="dropdown-content">
+        <a href="Louvre.php">Louvre Museum</a>
+        <a href="Uffizi.php">Uffizi Gallery</a>
+        <a href="BM.php">The British Museum</a>
+        <a href="MMA.php">The Museum of Modern Art</a>
+      </div>
+  </div>
+    <a href="AboutUs.php">ABOUT US</a> 
+    <a href="Logout.php">LOG OUT</a>
+    </div>
+    
+    <div class = "header">
+      <h1>Users Dashboard</h1>
+    </div>
+    </body>
+    </html>
