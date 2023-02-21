4abbb32ea72a62ffae1ef89dd7a2f0423ee9d2b9
anjezagllareva <anjezagllarevaa@gmail.com>
2023-02-21 22:44:21 +0100

    products

---
+++ Views/Products.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Museum</title>
+    <link rel="stylesheet" href="ProductsStyle.php">
+</head>
+<body>
+  <header><a href="HomeUser.php">MuseumEX</a></header>
+<div class="navbar">
+    <a href="HomeUser.php">HOME</a>
+    <a href="Products.php">PRODUCTS</a>
+    <div class="dropdown">
+      <button class="dropbtn">MUSEUMS
+      </button>
+      <div class="dropdown-content">
+        <a href="Louvre.php">Louvre Museum</a>
+        <a href="Uffizi.php">Uffizi Gallery</a>
+        <a href="BM.php">The British Museum</a>
+        <a href="MMA.php">The Museum of Modern Art</a>
+      </div>
+  </div>
+    <a href="Contact.php">CONTACT US</a> 
+    <a href="AboutUs.php">ABOUT US</a> 
+    <a href="Logout.php">LOGOUT</a> 
+    </div>
+    
+    <div class = "header">
+      <h1>Products</h1>
+    </div>
+    </body>
+    </html>
