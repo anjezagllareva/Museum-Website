0d760c59fe69cef81e600a05e90ef7ed43e9fd67
anjezagllareva <anjezagllarevaa@gmail.com>
2023-02-25 20:16:28 +0100

    museums admin service

---
+++ Services/MuseumsAdminService.php
@@ -0,0 +1,24 @@
+<?php 
+include_once '../../Database/DatabaseConnection.php';
+
+class MuseumsAdminService{
+    private $connection;
+
+    function __construct()
+    {
+        $databaseConnection = new DatabaseConnection;
+        $this->connection = $databaseConnection->startConnection();
+    }
+    
+    function getMuseumByName($name){
+        $databaseConnection = $this->connection;
+
+        $sql = "SELECT * FROM museums WHERE name='$name'";
+
+        $statement = $databaseConnection->query($sql);
+        $museum = $statement->fetch();
+
+        return $museum;
+    }
+}
+?>
