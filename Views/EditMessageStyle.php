8a6946b1064c344b3476c9db08de23e6672b0325
anjezagllareva <anjezagllarevaa@gmail.com>
2023-02-25 14:29:05 +0100

    style i edit message

---
+++ Views/EditMessageStyle.php
@@ -0,0 +1,41 @@
+
+<?php
+header('Content-type: text/css; charset:UTF-8');
+?>
+body{
+    background-color: #171c24;
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 1050px;
+    padding:200px;
+    padding-top: 25px;
+    padding-bottom: 25px;
+    width:650px;
+    margin-left:100px;
+  }
+  input[type=text], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+input[type=submit] {
+  width: 100%;
+  background-color: #997300;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  font-family: 'Times New Roman', Times, serif;
+  font-size:18px;
+}
+div {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
